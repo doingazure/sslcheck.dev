b49fdc1dda613c6a22ca77df2698b549265ec65e
Bill Wilder <codingoutloud@gmail.com>
2024-05-16 20:48:21 -0400

    moar testing

--- src/handlers.js
+++ src/handlers.js
@@ -1,5 +1,17 @@
 //  function that can be called from HTML when the user clicks on the button
-function handleCheckSsl(domain) {
+async function handleCheckSsl(domain) {
    console.log('Button clicked ' + domain);
+   alert('Button clicked ' + domain);
+
+   const api = `http://localhost:7071/api/SslCheck?domain=${domain}`;
+
+   try {
+      var json = await fetch(api);
+      return json;
+   } catch (error) {
+      console.error(error);
+      return error;
+   }
+
 }
 
